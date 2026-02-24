4ddb67db521b1c96a4e821fdd78c55d5c982878a
Юрий <yurikbc3@gmail.com>
2026-02-24 18:45:33 +0800

    Spring Boot First

---
+++ src/main/java/org/skypro/skyshopOne/SkyshopOneApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.skyshopOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyshopOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyshopOneApplication.class, args);
+	}
+
+}
